a92d4987643f2bbe0530cd3bb4018cf1ed266197
jiayu8302 <32969607+jiayu8302@users.noreply.github.com>
2018-05-03 00:14:37 +0930

    Add files via upload

---
+++ Valid_Parenthless.java
@@ -0,0 +1,27 @@
+/*
+https://leetcode.com/problems/valid-parentheses/description/
+20. Valid Parenthless
+ */
+import java.util.Stack;
+
+public class Solution {
+    public static void main(String[] args) {
+        String s = "";
+        System.out.println(isValid(s));
+    }
+    static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c == '('){
+                stack.push(')');
+            }else if(c == '{'){
+                stack.push('}');
+            }else if(c == '['){
+                stack.push(']');
+            }else if(stack.isEmpty() || stack.pop() != c){   //isEmpty(): s = ']'
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
