5e291406a64074d395545f7e8f8dfb12ca2b6588
jiayu8302 <32969607+jiayu8302@users.noreply.github.com>
2018-06-18 17:51:47 +0800

    Add files via upload

---
+++ 482. License Key Formatting.java
@@ -0,0 +1,24 @@
+/**
+* https://leetcode.com/problems/license-key-formatting/description/
+* 482. License Key Formatting
+* @author Jiayu Yao
+ */
+public class Solution {
+    public String licenseKeyFormatting(String S, int K) {
+        StringBuilder result = new StringBuilder();
+        int count = 0;
+        for(int i = S.length() - 1; i > -1; i--){
+            if(S.charAt(i) == '-')  continue;
+            else if(count < K){
+                result.append(S.substring(i, i + 1));
+                count++;
+            }else{
+                count = 0;
+                result.append("-");
+                i++;
+            }
+        }
+        result = result.reverse();
+        return result.toString().toUpperCase();
+    }
+}
