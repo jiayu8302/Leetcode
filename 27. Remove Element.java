f1fe98ac39d0c684f0fa5032bfce177e7aa22586
jiayu8302 <32969607+jiayu8302@users.noreply.github.com>
2018-06-18 17:51:59 +0800

    Add files via upload

---
+++ 27. Remove Element.java
@@ -0,0 +1,17 @@
+/**
+* https://leetcode.com/problems/remove-element/description/
+* 27. Remove Element
+* @author Jiayu Yao
+**/
+class Solution {
+    public int removeElement(int[] nums, int val) {
+    int i = 0;
+    for (int j = 0; j < nums.length; j++) {
+        if (nums[j] != val) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    return i;
+   } 
+}
